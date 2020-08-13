f23a6f6471d522db60cde1432c8a1cbe765baceb
shamal-17 <62537852+shamal-17@users.noreply.github.com>
2020-08-13 15:26:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,79 @@
+import React, { Component} from 'react';
+import HomeIcon from "@material-ui/icons/Home";
+import ReceiptIcon from "@material-ui/icons/Receipt";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import DesktopWindowsIcon from "@material-ui/icons/DesktopWindows";
+import SettingsIcon from "@material-ui/icons/Settings";
+import Sidebar from "./Sidebar";
+import Dialog from "./component/Dialog";
+
+
+function onClick(e, item) {
+  window.alert(JSON.stringify(item, null, 2));
+}
+
+const items = [
+  { name: "home", label: "Home", Icon: HomeIcon },
+  {
+    name: "application",
+    label: "Application",
+    Icon: ReceiptIcon,
+    items: [
+      { name: "statements", label: "Statements", onClick },
+      { name: "reports", label: "Reports", onClick }
+    ]
+  },
+  "divider",
+  {
+    name: "settings",
+    label: "Settings",
+    Icon: SettingsIcon,
+    items: [
+      { name: "profile", label: "Profile" },
+      { name: "insurance", label: "Insurance", onClick },
+      "divider",
+      {
+        name: "notifications",
+        label: "Notifications",
+        Icon: NotificationsIcon,
+        items: [
+          { name: "email", label: "Email", onClick },
+          {
+            name: "desktop",
+            label: "Desktop",
+            Icon: DesktopWindowsIcon,
+            items: [
+              { name: "schedule", label: "Schedule" },
+              { name: "frequency", label: "Frequency" }
+            ]
+          },
+          { name: "sms", label: "SMS" }
+        ]
+      }
+    ]
+  }
+];
+
+
+class App extends Component {
+
+  state={
+    isOpen:false
+  }
+  
+
+  render(){
+  return (
+
+    <div>
+      <Sidebar
+       items={items} />
+      </div>
+
+
+  );
+
+  }
+}
+
+export default App;
